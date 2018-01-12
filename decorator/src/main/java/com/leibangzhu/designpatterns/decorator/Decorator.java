7681864010156d353fee26a8d4b3651c43fa9aa8
雷威 <leiwei@dingdatech.com>
2018-01-12 16:37:12 +0800

    cc

--- decorator/src/main/java/com/leibangzhu/designpatterns/decorator/Decorator.java
+++ decorator/src/main/java/com/leibangzhu/designpatterns/decorator/Decorator.java
@@ -6,14 +6,4 @@ public abstract class Decorator implements Drink {
     public Decorator(Drink drink){
         this.drink = drink;
     }
-
-//    @Override
-//    public String getDescription() {
-//        return "Just decorator ...";
-//    }
-
-//    @Override
-//    public double getPrice() {
-//        return 0;
-//    }
 }
