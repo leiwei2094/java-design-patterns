a48b36edec3165c8c6925f8df98fd28c9ee9bc52
雷威 <leiwei@dingdatech.com>
2018-01-12 16:08:48 +0800

    cc

--- chain-filter/src/main/java/com/leibangzhu/designpatterns/chainfilter/filter/Filter.java
+++ chain-filter/src/main/java/com/leibangzhu/designpatterns/chainfilter/filter/Filter.java
@@ -5,5 +5,10 @@ import com.leibangzhu.designpatterns.chainfilter.Request;
 import com.leibangzhu.designpatterns.chainfilter.Result;
 
 public interface Filter {
+
+    // before filter
+    // Result result = handler.handle(request)
+    // after filter
+    // return result
     Result handle(Handler handler, Request request);
 }
